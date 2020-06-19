3f223503088b74b3509cfa19c7f66c49046954b9
kutluduman <kutluduman@gmail.com>
2020-06-19 01:28:01 -0400

    "Odd function"

---
+++ odd.js
@@ -0,0 +1,14 @@
+const isOdd = function (num) {
+
+  return num % 2 === 1;
+
+
+}
+
+
+const isTenOdd = isOdd(10);
+const isElevenOdd = isOdd(11);
+
+
+console.log(isElevenOdd);
+console.log(isTenOdd);
