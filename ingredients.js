2fdb6f14d5a2860781ca83a8e724b6876a3d6a2e
kutluduman <kutluduman@gmail.com>
2020-06-19 22:18:34 -0400

    "while, for and reverse for loop"

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0 ;
+while(i < ingredients.length) {
+console.log(ingredients[i]);
+i++;
+
+}
+
+
+for(let x =0 ; x<ingredients.length ; x++) {
+
+  console.log(ingredients[x]);
+}
+
+
+for(let y = ingredients.length-1 ; y>=0 ; y--) {
+
+  console.log(ingredients[y]);
+}
